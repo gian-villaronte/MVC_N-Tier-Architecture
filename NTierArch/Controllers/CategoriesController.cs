79a05dfa27b5e0572c9a9f4452398d850720f375
Gian Villaronte <111197673+gian-villaronte@users.noreply.github.com>
2023-09-30 13:18:09 -0400

    Add files via upload

---
+++ NTierArch/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GE = Global.Entity;
+using Business.Layer;
+using Business.Layer.Interface;
+
+namespace NTierArch.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryBL _category;
+
+        public CategoriesController(ICategoryBL category)
+        {
+            _category = category;
+        }
+        public IActionResult Index()
+        {
+            List<GE::Category> categories = _category.GetCategories();
+            return View(categories);
+        }
+    }
+}
